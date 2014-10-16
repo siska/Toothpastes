b194ec53ee715d1d4499109f6826683a5410b604
Ryan Siska <rsiska1@gmail.com>
2014-10-16 10:50:12 -0500

    Initial Commit

---
+++ ToothpastesIHaveLovedAndAdored/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToothpastesIHaveLovedAndAdored
+//
+//  Created by S on 10/16/14.
+//  Copyright (c) 2014 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
